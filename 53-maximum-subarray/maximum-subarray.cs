18e8039062d781bebeaf8813580c7b9e5766b12e
Uroš Grahovac <50529396+uros002@users.noreply.github.com>
2026-01-03 12:29:07 +0100

    Time: 1 ms (100.00%) | Memory: 63.8 MB (59.80%) - LeetSync

---
+++ 53-maximum-subarray/maximum-subarray.cs
@@ -0,0 +1,14 @@
+public class Solution {
+    public int MaxSubArray(int[] nums) {
+        var currentSum = nums[0];
+        var maxSum = nums[0];
+
+        for(int i = 1; i < nums.Length; i++){
+            currentSum = Math.Max(nums[i],currentSum + nums[i]);
+            maxSum = Math.Max(maxSum,currentSum);
+        }
+
+        return maxSum;
+
+    }
+}
